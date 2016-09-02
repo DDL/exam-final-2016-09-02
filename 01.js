28173632cad0dfaa684b867c1365d3b1c052ee01
DDL <doczi.david.daci@gmail.com>
2016-09-02 09:52:33 +0200

    first version

--- 01.js
+++ 01.js
@@ -1,5 +1,6 @@
 // Create a function that counts the cars by colors
 // With an output like: {red: 3, yellow: 2, white: 1, green: 1}
+'use strict';
 
 
 const cars = [{
@@ -25,3 +26,27 @@ const cars = [{
     type: 'Trabant'
 }];
 
+function count_cars_by_color(cars) {
+    var red = 0;
+    var yellow = 0;
+    var white = 0;
+    var green = 0;
+    cars.forEach(function (e) {
+        if (e.color  === 'red') {
+            red++;
+        }
+        if (e.color  === 'yellow') {
+            yellow++;
+        }
+        if (e.color  === 'white') {
+            white++;
+        }
+        if (e.color  === 'green') {
+            green++;
+        }
+    });
+    var output = {'red' : red, 'yellow' : yellow, 'white' : white, 'green' : green}
+    console.log(output);
+}
+
+count_cars_by_color(cars);
